c45b1785212ab1f8eb3213584d714382a7b40a0e
Marek Zelinka <mzelinka17@gmail.com>
2024-08-18 14:59:35 +0200

    solve Exercise 9.17

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getAllDiaries } from './services/diary';
+import { createDiary, getAllDiaries } from './services/diary';
 import type { NonSensitiveDiaryEntry } from './types';
 
 function App() {
@@ -11,6 +11,45 @@ function App() {
 
   return (
     <>
+      <h2>Add new entry</h2>
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+
+          const form = event.currentTarget;
+          const formData = new FormData(form);
+
+          const newEntry = await createDiary({
+            date: String(formData.get('date')),
+            visibility: String(formData.get('visibility')),
+            weather: String(formData.get('weather')),
+            comment: String(formData.get('comment')),
+          });
+          setDiaries((diaries) => diaries.concat(newEntry));
+
+          form.reset();
+        }}
+      >
+        <div>
+          <label htmlFor="date">date</label>
+          <input type="date" name="date" id="date" />
+        </div>
+        <div>
+          <label htmlFor="visibility">visibility</label>
+          <input type="text" name="visibility" id="visibility" />
+        </div>
+        <div>
+          <label htmlFor="weather">weather</label>
+          <input type="text" name="weather" id="weather" />
+        </div>
+        <div>
+          <label htmlFor="comment">comment</label>
+          <textarea name="comment" id="comment" />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
       <h2>Diary entries</h2>
       {diaries.map((entry) => (
         <div key={entry.id}>
--- src/services/diary.ts
+++ src/services/diary.ts
@@ -1,9 +1,18 @@
 import axios from 'axios';
-import type { NonSensitiveDiaryEntry } from '../types';
+import {
+  DiaryEntry,
+  type NewDiaryEntry,
+  type NonSensitiveDiaryEntry,
+} from '../types';
 
 const instance = axios.create({ baseURL: '/api/diaries' });
 
 export const getAllDiaries = async () => {
   const response = await instance.get<NonSensitiveDiaryEntry[]>('/');
+  return response.data;
+};
+
+export const createDiary = async (newEntry: NewDiaryEntry) => {
+  const response = await instance.post<DiaryEntry>('/', newEntry);
   return response.data;
 };
